4387c315daa8115d1258c695b92f580ac040c115
Zachary Westerman <westerman.zachary@gmail.com>
2025-08-16 16:29:29 -0500

    add docs

--- src/util/downloadQueue.hpp
+++ src/util/downloadQueue.hpp
@@ -6,24 +6,53 @@
 
 namespace util {
 
+/**
+ * @brief A struct representing a download.
+ */
 struct Download {
+	/// The destination file path.
 	fs::Path file;
+	/// The network request associated with the download.
 	net::Request request;
+	/// The unique identifier for the download.
 	int id;
 };
 
+/**
+ * @brief A class for managing a queue of downloads.
+ */
 class DownloadQueue {
 	std::vector<Download *> downloads;
 
 public:
+	/// Destructor.
 	~DownloadQueue();
 
+	/**
+	 * @brief Add a download to the queue.
+	 * @param file The destination file path.
+	 * @param hostname The hostname for the download.
+	 * @param url The URL for the download.
+	 * @return The unique identifier for the download.
+	 */
 	int download(const fs::Path &file, const String &hostname, const String &url);
 
+	/**
+	 * @brief Check if a download is finished.
+	 * @param id The unique identifier for the download.
+	 * @return True if the download is finished, false otherwise.
+	 */
 	bool finished(int id) const;
 
+	/**
+	 * @brief Process the download queue, appending an available data to the relevant files.
+	 * @note This does not remove finished downloads from the queue.
+	 */
 	void process();
 
+	/**
+	 * @brief Clean up finished downloads.
+	 */
 	void cleanup();
 };
 
