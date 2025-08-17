68a8455a861385a523a381e0cdf487514957a32d
ZacharyWesterman <ZacharyWesterman@users.noreply.github.com>
2025-08-17 02:13:05 +0000

    deploy: 089efb8ffdb26d602faccf25e1bcc13dcffd9a00

--- classutil_1_1DownloadQueue.js
+++ classutil_1_1DownloadQueue.js
@@ -3,7 +3,7 @@ var classutil_1_1DownloadQueue =
     [ "~DownloadQueue", "classutil_1_1DownloadQueue.html#ad995f5056ab8682b02c49d2004a4a6fa", null ],
     [ "begin", "classutil_1_1DownloadQueue.html#ad89f13866d3a0e225403c11a647b190a", null ],
     [ "cleanup", "classutil_1_1DownloadQueue.html#ae4175efe328f8a63cac5ec35f2c5c6a1", null ],
-    [ "download", "classutil_1_1DownloadQueue.html#a9844305b5136b0435a4ebe5c8e868fea", null ],
+    [ "download", "classutil_1_1DownloadQueue.html#adad9c4b16dc1d705c5dfafc84e3259c0", null ],
     [ "end", "classutil_1_1DownloadQueue.html#a082bcc4e0445b7553e34e33259b049ac", null ],
     [ "finished", "classutil_1_1DownloadQueue.html#adb3735dca1e6a5d6f1e3a5e171edc75b", null ],
     [ "process", "classutil_1_1DownloadQueue.html#a2aa089b8dab92c75f62acf2724baf55d", null ]
--- search/functions_4.js
+++ search/functions_4.js
@@ -3,7 +3,7 @@ var searchData=
   ['data_0',['data',['../classnet_1_1Request.html#a70c05be80b609d651669d9866568a3be',1,'net::Request']]],
   ['disconnect_1',['disconnect',['../fs_8hpp.html#ab47ec5eeb5a0ea0dff1175edc1586b28',1,'fs::disconnect()'],['../net_8hpp.html#a4035db0c991825c38f48e32bc2078fa9',1,'net::disconnect()']]],
   ['done_2',['done',['../classnet_1_1Request.html#ad1f0452724b2aa03711393327e669b39',1,'net::Request']]],
-  ['download_3',['download',['../classutil_1_1DownloadQueue.html#a9844305b5136b0435a4ebe5c8e868fea',1,'util::DownloadQueue']]],
+  ['download_3',['download',['../classutil_1_1DownloadQueue.html#adad9c4b16dc1d705c5dfafc84e3259c0',1,'util::DownloadQueue']]],
   ['downloaded_4',['downloaded',['../classnet_1_1Request.html#a36cb78753d01bda9a8c3e61094ea3896',1,'net::Request']]],
   ['downloadqueueiter_5',['DownloadQueueIter',['../classutil_1_1DownloadQueueIter.html#a24de157d7443178ae941920e6942be84',1,'util::DownloadQueueIter']]],
   ['duration_6',['duration',['../classaudio_1_1Player.html#aaba993a925672822a647365314b92ed3',1,'audio::Player']]]
